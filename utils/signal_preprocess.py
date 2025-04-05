514a121379e777911c9f5ef2316ffa48f7b27fce
QiudiHe <130117975+qdh-2002@users.noreply.github.com>
2025-04-05 12:36:22 -0700

    Update signal_preprocess.py

--- utils/signal_preprocess.py
+++ utils/signal_preprocess.py
@@ -1,1 +1,9 @@
+import mne
+import numpy as np
+from scipy.signal import resample
 
+def preprocess_edf(file_path, target_fs=128):
+    raw = mne.io.read_raw_edf(file_path, preload=True)
+    raw.resample(target_fs)
+    data = raw.get_data()
+    return data.T 
